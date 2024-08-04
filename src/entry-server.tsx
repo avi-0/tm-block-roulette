e4a688f057dfc6ebfa934437ce053b0a95e93844
avi <db0zmail@gmail.com>
2024-08-04 18:01:25 -0300

    base tag

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -2,20 +2,24 @@
 import { createHandler, StartServer } from "@solidjs/start/server";
 
 export default createHandler(() => (
-  <StartServer
-    document={({ assets, children, scripts }) => (
-      <html lang="en">
-        <head>
-          <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
-          {assets}
-        </head>
-        <body>
-          <div id="app">{children}</div>
-          {scripts}
-        </body>
-      </html>
-    )}
-  />
+    <StartServer
+        document={({ assets, children, scripts }) => (
+            <html lang="en">
+                <head>
+                    <base href="/tm-block-roulette/" />
+                    <meta charset="utf-8" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1"
+                    />
+                    <link rel="icon" href="/favicon.ico" />
+                    {assets}
+                </head>
+                <body>
+                    <div id="app">{children}</div>
+                    {scripts}
+                </body>
+            </html>
+        )}
+    />
 ));
